3848322860c1e5af0d55ef577281578c9fe5da1b
chizhcode <91279744+chizhcode@users.noreply.github.com>
2025-01-27 12:10:21 +0300

    WIP

---
+++ lua/custom/plugins/fun.lua
@@ -0,0 +1,14 @@
+return {
+  --funny BS
+--{ 'eandrju/cellular-automaton.nvim' },
+--{ 'seandewar/bad-apple.nvim' },
+--{ 'AndrewRadev/typewriter.vim' },
+--{ 'AndrewRadev/dealwithit.vim' },
+--{ 'AndrewRadev/discotheque.vim' },
+--{ 'seandewar/killersheep.nvim' },
+--{
+--  'giusgad/pets.nvim',
+--  dependencies = { 'MunifTanjim/nui.nvim', 'giusgad/hologram.nvim' },
+--},
+--{ 'muratsat/rickroll-nvim' },
+}
